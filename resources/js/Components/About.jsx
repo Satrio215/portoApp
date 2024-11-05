7c389349f2c713ccad4b44963633354f1f9af380
Satrio215 <satrio.ajinoto@gmail.com>
2024-11-05 20:41:14 +0700

    landing

--- resources/js/Components/About.jsx
+++ resources/js/Components/About.jsx
@@ -4,7 +4,7 @@ import GitHubCalendar from 'react-github-calendar';
 
 const content = [
     "About Me",
-    "Welcome to my portfolio! I'm a dedicated backend developer with a strong passion for coding and solving complex problems through efficient and scalable solutions. With a solid background in languages like PHP, Java, and JavaScript, I enjoy working on innovative projects that push the boundaries of technology. I'm always eager to learn new tools, frameworks, and methodologies to stay updated with the latest industry trends. Whether it’s improving performance, optimizing databases, or integrating APIs, I thrive on crafting clean, maintainable code. Feel free to explore my projects, and don't hesitate to connect with me for collaborations or discussions!",
+    "Welcome to my portfolio! I'm a student at Brawijaya University, Faculty of Computer Science, and a dedicated backend developer with a strong passion for coding and solving complex problems through efficient, scalable solutions. With a solid background in languages like PHP, Java, and JavaScript, I’m now expanding my skills to become a full-stack developer, diving into both frontend and backend technologies. I enjoy working on innovative projects that push the boundaries of technology and am always eager to learn new tools, frameworks, and methodologies to stay updated with the latest industry trends. Whether it’s improving performance, optimizing databases, building intuitive interfaces, or integrating APIs, I thrive on crafting clean, maintainable code. Feel free to explore my projects, and don't hesitate to connect with me for collaborations or discussions!",
 ];
 
 const About = ({ pengalamans }) => {
@@ -20,11 +20,11 @@ const About = ({ pengalamans }) => {
     return (
         <section ref={target} className="w-full relative rounded-t-3xl h-[300vh] bg-black justify-center">
             <div className="h-screen sticky top-0 flex-col items-center justify-center py-8 px-8">
-                <div className="max-w-7xl mx-auto lg:px-0 px-2">
+                <div className="mx-auto lg:px-32">
                     {content.map((item, lineIndex) => {
                         const words = item.split(" ");
                         return (
-                            <p key={lineIndex} className={` ${lineIndex === 2 ? 'text-end  w-fit ms-auto' : ''} flex flex-wrap font-bold lg:mb-4 ${lineIndex === 0 || lineIndex === 2 ? 'font-kampungbudaya text-lg lg:text-5xl font-bold' : "font-jakarta text-base lg:text-lg"}`}>
+                            <p key={lineIndex} className={` ${lineIndex === 2 ? 'text-end  w-fit ms-auto' : ''} flex flex-wrap font-bold lg:mb-4 ${lineIndex === 0 || lineIndex === 2 ? 'text-4xl lg:text-5xl font-bold' : "text-base lg:text-lg"}`}>
                                 {words.map((word, wordIndex) => {
                                     const start = wordCounter / totalWords;
                                     const end = (wordCounter + 1) / totalWords;
@@ -44,8 +44,8 @@ const About = ({ pengalamans }) => {
                     })}
                 </div>
 
-                <div className="py-8 px-8 ">
-                    <h3 className="text-white font-bold text-2xl mb-4">Experience</h3>
+                <div className="md:px-32 sm:px-8 py-8">
+                    <h3 className="text-white font-bold text-4xl mb-4">Experience</h3>
                     <ul className="text-white flex flex-wrap gap-4">
                         {pengalamans.map((pengalaman) => (
                             <li 
