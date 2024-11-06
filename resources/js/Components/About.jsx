b3079d327d32741d2529bda2695a1c8f6b82a7b6
Satrio215 <satrio.ajinoto@gmail.com>
2024-11-06 14:11:11 +0700

    experience

--- resources/js/Components/About.jsx
+++ resources/js/Components/About.jsx
@@ -4,7 +4,7 @@ import GitHubCalendar from 'react-github-calendar';
 
 const content = [
     "About Me",
-    "Welcome to my portfolio! I'm a student at Brawijaya University, Faculty of Computer Science, and a dedicated backend developer with a strong passion for coding and solving complex problems through efficient, scalable solutions. With a solid background in languages like PHP, Java, and JavaScript, I’m now expanding my skills to become a full-stack developer, diving into both frontend and backend technologies. I enjoy working on innovative projects that push the boundaries of technology and am always eager to learn new tools, frameworks, and methodologies to stay updated with the latest industry trends. Whether it’s improving performance, optimizing databases, building intuitive interfaces, or integrating APIs, I thrive on crafting clean, maintainable code. Feel free to explore my projects, and don't hesitate to connect with me for collaborations or discussions!",
+    "Welcome to my portfolio! I’m a Computer Science student at Brawijaya University, focused on backend development and passionate about creating efficient, scalable solutions. With expertise in PHP, Java, and JavaScript, I’m expanding my skills toward full-stack development, exploring both frontend and backend technologies. I enjoy tackling challenging projects, improving performance, optimizing databases, and building clean, maintainable code. Feel free to check out my work or connect for collaborations and discussions!",
 ];
 
 const About = ({ pengalamans }) => {
@@ -24,7 +24,7 @@ const About = ({ pengalamans }) => {
                     {content.map((item, lineIndex) => {
                         const words = item.split(" ");
                         return (
-                            <p key={lineIndex} className={` ${lineIndex === 2 ? 'text-end w-fit ms-auto' : ''} flex flex-wrap font-bold lg:mb-4 ${lineIndex === 0 || lineIndex === 2 ? 'text-4xl lg:text-5xl font-bold' : "text-base lg:text-lg"}`}>
+                            <p key={lineIndex} className={` ${lineIndex === 2 ? 'text-end w-fit ms-auto' : ''} flex flex-wrap font-bold lg:mb-4 ${lineIndex === 0 || lineIndex === 2 ? 'text-4xl lg:text-6xl font-bold' : "text-base lg:text-lg"}`}>
                                 {words.map((word, wordIndex) => {
                                     const start = wordCounter / totalWords;
                                     const end = (wordCounter + 1) / totalWords;
@@ -44,29 +44,29 @@ const About = ({ pengalamans }) => {
                     })}
                 </div>
 
-                <div className="md:px-32 sm:px-8 py-8">
+                <div className="md:px-32 sm:px-8 py-2">
                     <h3 className="text-white font-bold text-4xl mb-4 pb-4">Experience</h3>
                     <div className="marqueeWrapper overflow-hidden">
                         <div className="marquee">
-                            <div className="marquee__content" style={{ '--duration': '30s' }}>
-                                {pengalamans.map((pengalaman) => (
+                            <div className="marquee__content pr-4" style={{ '--duration': '80s' }}>
+                                {pengalamans.map((pengalaman, index) => (
                                     <li
                                         key={pengalaman.id}
-                                        className="mb-4 p-4 border border-gray-500 rounded-lg flex-grow md:flex-grow-0 md:w-5/12 lg:w-3/12"
+                                        className={`mb-4 p-4 border border-gray-500 rounded-lg flex-grow md:flex-grow-0 md:w-5/12 lg:w-3/12 ${index < pengalamans.length - 1 ? 'mr-4' : ''}`}
                                     >
-                                        <h4 className="font-semibold">{pengalaman.judul}</h4>
+                                        <h4 className="font-semibold pb-4">{pengalaman.judul}</h4>
                                         <p className="font-extralight text-sm">{pengalaman.mulai} - {pengalaman.selesai}</p>
                                         <p>{pengalaman.keterangan}</p>
                                     </li>
                                 ))}
                             </div>
-                            <div className="marquee__content" style={{ '--duration': '30s' }} aria-hidden="true">
-                                {pengalamans.map((pengalaman) => (
+                            <div className="marquee__content pr-4 " style={{ '--duration': '80s' }} aria-hidden="true">
+                                {pengalamans.map((pengalaman, index) => (
                                     <li
                                         key={pengalaman.id}
-                                        className="mb-4 p-4 border border-gray-500 rounded-lg flex-grow md:flex-grow-0 md:w-5/12 lg:w-3/12"
+                                        className={`mb-4 p-4 border border-gray-500 rounded-lg flex-grow md:flex-grow-0 md:w-5/12 lg:w-3/12 ${index < pengalamans.length - 1 ? 'mr-4' : ''}`}
                                     >
-                                        <h4 className="font-semibold">{pengalaman.judul}</h4>
+                                        <h4 className="font-semibold pb-4">{pengalaman.judul}</h4>
                                         <p className="font-extralight text-sm">{pengalaman.mulai} - {pengalaman.selesai}</p>
                                         <p>{pengalaman.keterangan}</p>
                                     </li>
@@ -77,7 +77,7 @@ const About = ({ pengalamans }) => {
                 </div>
 
                 <div className="py-8 px-8">
-                    <div className="flex justify-center w-full md:w-auto rounded-xl px-6 py-8 border border-gray-400 md:max-w-4xl mx-auto">
+                    <div className="flex justify-center w-full md:w-auto rounded-xl px-6 py-8 border border-gray-500 md:max-w-4xl mx-auto">
                         <GitHubCalendar style={{ color: 'white' }} username="Satrio215" />
                     </div>
                 </div>
